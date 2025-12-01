9437bf95ce8bce43d1ae52907c5854e07b852b87
Robert <rnickens3@gmail.com>
2025-12-01 11:31:25 -0600

    Add sentence analysis functions
    Implemented functions to count vowels, consonants, punctuation, and words in a sentence.

---
+++ sentenceAnalyzer.js
@@ -0,0 +1,66 @@
+/*
+This assignment was completed December 1st, 2025.
+Utilizing loops, I created several functions that will count letters, symbols, and words in any sentence.
+Conceps learned: Loops & Iteration and regex [/\s+/]
+*/
+
+//This function will read through the vowels in a sentence and outputs how many there are.
+function getVowelCount(sentence) {
+  const vowels = "aeiou";
+  let count = 0;
+
+  for (const char of sentence.toLowerCase()) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const vowelCount = getVowelCount("Apples are tasty fruits");
+console.log(`Vowel Count: ${vowelCount}`);
+
+//Functoin will read through a sentence and count how many consonants there are.
+function getConsonantCount(sentence) {
+  const consonants = "bcdfghjklmnpqrstvwxyz"; 
+  let count = 0;
+
+  for (const char of sentence.toLowerCase()) {
+    if (consonants.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const consonantCount = getConsonantCount("Coding is fun");
+console.log(`Consonant Count: ${consonantCount}`);
+
+//Reads through a sentence adn counts how many symbols there are.
+function getPunctuationCount(sentence) {
+  const punctuations = ".,!?;:-()[]{}\"'–";
+  let count = 0;
+
+  for (const char of sentence) {
+    if (punctuations.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const punctuationCount = getPunctuationCount("WHAT?!?!?!?!?");
+console.log(`Punctuation Count: ${punctuationCount}`);
+
+//Reads through a sentence and counts how many words there are. 
+function getWordCount(sentence) {
+  if (sentence.trim() === '') {
+    return 0;
+  }
+  
+  const words = sentence.trim().split(/\s+/);
+  return words.length;
+}
+
+const wordCount = getWordCount("I love freeCodeCamp");
+console.log(`Word Count: ${wordCount}`)
